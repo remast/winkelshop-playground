516badb9b31923820efa4be514158cf34003fcd2
Jan Stamer <914776+remast@users.noreply.github.com>
2026-05-28 17:15:54 +0200

    add

---
+++ winkelshop-backend/src/test/java/remast/winkelshop/order/control/CartServiceTest.java
@@ -0,0 +1,12 @@
+package remast.winkelshop.order.control;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartServiceTest {
+
+    @Test
+    void addItem() {
+    }
+}
